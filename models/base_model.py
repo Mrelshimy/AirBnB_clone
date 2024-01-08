6c2c85de5133810c08c0ad311d9bf28ccfb8c990
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-08 20:59:00 +0200

    Update for Update_now and save()

--- models/base_model.py
+++ models/base_model.py
@@ -12,7 +12,7 @@ class BaseModel():
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = BaseModel.save(self)
+            self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -29,7 +29,7 @@ class BaseModel():
 
     def save(self):
         """Update Updated_at to current datetime"""
-        return datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Return a Dictionary representation of an object"""
