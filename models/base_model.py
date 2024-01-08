aeac3a437eeabf3ccaf85082e87db414d1495ccf
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-08 13:36:40 +0200

    Adding Task 4 update to base model (Create object from dict)

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ from datetime import datetime
 class BaseModel():
     """Base Model Class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = BaseModel.save(self)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = BaseModel.save(self)
+        else:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                if key == '__class__':
+                    continue
+                else:
+                    setattr(self, key, value)
 
     def __str__(self):
         """Return String representation of an object"""
