2d969687aa2d94600554933ce5499716c3a7b375
nono <nour98nour@gmail.com>
2024-01-09 11:57:44 +0200

    created 5 more classes!

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 import json
 from models.base_model import BaseModel
 from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+
 import datetime
 """ File Storage Module """
 
---
+++ models/review.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Review Class Module"""
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    The Review class inherits from the BaseModel class.
+    It represents a review with attributes for
+    place_id, user_id, and text.
+
+    Attributes:
+        place_id (str): The ID of the place being reviewed.
+        user_id (str): The ID of the user who wrote the review.
+        text (str): The content of the review.
+    """
+
+    place_id = ""
+    user_id = ""
+    text = ""
+
+
+    def __init__(self):
+        """
+        Constructor for the Review class.
+        """
+        pass
