70845d0d90c8cac7dc872a4990b3964257f5be3f
nour-nono <115364839+nour-nono@users.noreply.github.com>
2024-01-12 23:35:29 +0200

    Update file_storage.py moved module documentation to the top

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" File Storage Module """
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -7,9 +9,7 @@ from models.city import City
 from models.place import Place
 from models.review import Review
 from models.state import State
-
 import datetime
-""" File Storage Module """
 
 
 class FileStorage:
