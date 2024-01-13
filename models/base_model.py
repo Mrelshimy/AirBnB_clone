3f475041447c55feee7f99b5ecf89167f66d19f7
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-13 14:37:32 +0200

    updating basemodel updated at

--- models/base_model.py
+++ models/base_model.py
@@ -46,7 +46,7 @@ class BaseModel:
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
         else:
             for key, value in kwargs.items():
